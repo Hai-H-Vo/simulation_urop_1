afdda20b963feaa86e2d0b775bd3f02fb2c20e05
Hai-H-Vo <haivo@mit.edu>
2025-08-25 14:36:34 +0700

    Add files via upload

--- basis_repr_1.py
+++ basis_repr_1.py
@@ -1,5 +1,6 @@
 # IMPORTS
 
+import sys
 import numpy as onp
 import jax.numpy as np
 import optax
@@ -27,13 +28,15 @@ from simulator.render import render
 from simulator.dynamics import pedestrian, PedestrianState, StraightWall
 
 # POSITIONS AND VELOCITIES
-POS_NUMS = 2
-ANGLE_NUMS = 2
+# (250, 40, 0.1, 1.5, 3.0, 1., 1000)
+CYCLE_NUMS = 80
+POS_NUMS = 25
+ANGLE_NUMS = 4
 R = 0.1
 K = 1.5
 T_0 = 3.0
 V_2_MAG = 1.
-CYCLE_NUMS = 1000
+CYCLE_NUMS = 80
 
 positions = np.stack([np.linspace(0.3, 2., POS_NUMS), np.zeros([POS_NUMS,])], axis=1)
 angles = np.linspace(0, 1/2, ANGLE_NUMS)
@@ -41,8 +44,8 @@ v_2 = V_2_MAG * np.stack([np.cos(onp.pi * angles), np.sin(onp.pi * angles)], axi
 v_1 = np.zeros([ANGLE_NUMS, 2])
 
 # PARAMS INIT
-paral_weights = np.zeros([10, 10, 10])
-perpen_weights = np.zeros([10, 10, 10])
+paral_weights = np.ones([10, 10, 10])
+perpen_weights = np.ones([10, 10, 10])
 d_0 = 10.
 v_0 = 10.
 
@@ -62,22 +65,44 @@ def loss_fn(params, pos, v1, v2):
 start_learning_rate = 0.1
 optimizer = optax.adam(start_learning_rate)
 
-# PARAMS
-params = {'paral' : paral_weights,
-          'perpen' : perpen_weights,
-          'd0' : d_0,
-          'v0' : v_0}
-opt_state = optimizer.init(params)
+if __name__ == "__main__":
+    # PARAMS
+    try:
+        sys.stdout.write("Loading old checkpoint..." + "\n")
+        params_npz = np.load(f"basis_repr_1_P{POS_NUMS}_A{ANGLE_NUMS}_checkpoint.npz")
+        params = {'paral' : params_npz['paral'],
+                'perpen' : params_npz['perpen'],
+                'd0' : params_npz['d0'],
+                'v0' : params_npz['v0']}
+        start_loop = params_npz['cycle']
+        sys.stdout.write("Checkpoint loaded!" + "\n")
+    except:
+        sys.stdout.write("No checkpoint found, initializing params." + "\n")
+        params = {'paral' : paral_weights,
+                'perpen' : perpen_weights,
+                'd0' : d_0,
+                'v0' : v_0}
+        start_loop = 0
 
-# UPDATE LOOP
-for i in range(CYCLE_NUMS):
-  print(f"Current update loop: {i}/{CYCLE_NUMS}")
-  grads = grad(loss_fn)(params, positions, v_1, v_2)
-  # fails before this point
-  updates, opt_state = optimizer.update(grads, opt_state)
-  params = optax.apply_updates(params, updates)
+    opt_state = optimizer.init(params)
 
-# FloatingPointError: invalid value (nan) encountered in mul
-# When differentiating the code at the top of the callstack:
-# /Users/vohoanghai/Documents/MIT/UROP_1/Work/General Simuls/simulator/force.py:105:15 (general_force_generator.<locals>.general_force)
-# /Users/vohoanghai/Documents/MIT/UROP_1/Work/General Simuls/basis_repr_2.py:55:11 (_loss_fn)
+    sys.stdout.write(f"Initial loss: {loss_fn(params, positions, v_1, v_2)}" + "\n")
+
+    # UPDATE LOOP
+    for i in range(start_loop, CYCLE_NUMS):
+        grads = grad(loss_fn)(params, positions, v_1, v_2)
+        updates, opt_state = optimizer.update(grads, opt_state)
+        params = optax.apply_updates(params, updates)
+
+        if (i + 1) % 50 == 0 and (i + 1) != CYCLE_NUMS:
+            sys.stdout.write(f"Current update loop: {i+1}/{CYCLE_NUMS}" + "\n")
+            sys.stdout.write("Saving checkpoint..." + "\n")
+            np.savez(f"basis_repr_1_P{POS_NUMS}_A{ANGLE_NUMS}_checkpoint", paral=params['paral'], perpen=params['perpen'], d0=params['d0'], v0=params['v0'], cycle=i+1)
+            sys.stdout.write(f"Checkpointed loss: {loss_fn(params, positions, v_1, v_2)}" + "\n")
+
+
+    sys.stdout.write(f"Final loss: {loss_fn(params, positions, v_1, v_2)}" + "\n")
+
+    # SAVE RESULTS
+    np.savez(f"basis_repr_1_P{POS_NUMS}_A{ANGLE_NUMS}_C{CYCLE_NUMS}", paral=params['paral'], perpen=params['perpen'], d0=params['d0'], v0=params['v0'])
+    sys.stdout.write("Optimization complete!")
